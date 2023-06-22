adbeca90bce2a7a09c7380a1fb299a4a6ce86481
Yan <young69budda@gmail.com>
2023-06-22 17:02:49 +0300

    all done2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const moreBtn = document.querySelector('.load-more');
 
 const API_KEY = '37633155-9cc69d2c24fc24ba983caa040';
 const BASE_URL = 'https://pixabay.com/api/';
-let photoPerPage = 5;
+let photoPerPage = 40;
 let page = 1;
 
 moreBtn.style.display = 'none';
@@ -25,6 +25,7 @@ const onSubmit = async e => {
     gallery.innerHTML = '';
     const markup = card(response.data);
     gallery.insertAdjacentHTML('afterbegin', markup);
+    Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
     moreBtn.style.display = 'block';
 
     if (response.data.totalHits === 0) {
@@ -51,7 +52,7 @@ const onMoreBtn = async () => {
     const response = await axios.get(
       `${BASE_URL}?key=${API_KEY}&q=${searchInput}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${photoPerPage}&page=${page}`
     );
-    // gallery.innerHTML = '';
+
     const markup = card(response.data);
     gallery.insertAdjacentHTML('beforeend', markup);
 
@@ -60,7 +61,11 @@ const onMoreBtn = async () => {
         'Sorry, there are no images matching your search query. Please try again.'
       );
     }
-    moreBtn.style.display = 'block';
+    const pageQuantity = Math.ceil(response.data.totalHits / photoPerPage);
+    if (page === pageQuantity) {
+      moreBtn.style.display = 'none';
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
   } catch (error) {
     console.log(error);
   }
